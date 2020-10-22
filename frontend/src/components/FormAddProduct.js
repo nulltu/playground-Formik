6d2fc765a41caf71daed6a2b9601755bcc8391cb
nulltu <rusbent.matta@gmail.com>
2020-10-22 11:45:25 -0300

    add upload image frontend

--- frontend/src/components/FormAddProduct.js
+++ frontend/src/components/FormAddProduct.js
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const FormAddProducts = () => {
+
+    const [product, setProduct] = useState({
+        title: '', description: '', image:'', price: ''
+    })
+
+    const readInput = (e) => {
+        const value = e.target.name === 'image' ? e.target.files[0] : e.target.value
+        setProduct({
+            ...product,
+            [e.target.name]: value
+        })
+    }
+
+    console.log(product.image)
+
     return (
         <>
             <form action="">
-                <input type="text" placeholder="Title" />
-                <input type="text" placeholder="Description" />
-                <input type="text" placeholder="Images Links" />
-                <input type="number" placeholder="Price" />
+                <input type="text" name="title" placeholder="Title" onChange={readInput}/>
+                <input type="text" name="description" placeholder="Description" onChange={readInput}/>
+                <input type="file" name="image" onChange={readInput}/>
+                <input type="number" name="price" placeholder="Price" onChange={readInput}/>
                 <button>Send</button>
             </form>
         </>
